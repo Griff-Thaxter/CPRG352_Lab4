ecb65c5c5d88ee6395b0dbc536affd6e310810c3
Griff-Thaxter <sniffinisakuntt@gmail.com>
2022-06-02 10:26:31 -0600

    Finishing touches

--- src/java/servlets/NoteServlet.java
+++ src/java/servlets/NoteServlet.java
@@ -9,16 +9,15 @@ import javax.servlet.http.HttpServletResponse;
 import models.Note;
 
 public class NoteServlet extends HttpServlet {  
-    public int counter = 0;
-    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {     
         //Grabs whether the "Edit" link was clicked or not
         String edit = request.getParameter("edit");
+        System.out.println(edit);
         
         //Check if viewnote.jsp should be displayed
-        if(counter == 0) {
+        if(edit == null) {
             //Path for text file
             String path = getServletContext().getRealPath("/WEB-INF/note.txt");
             //Read the file
@@ -32,15 +31,12 @@ public class NoteServlet extends HttpServlet {
             
             request.setAttribute("note", note);
             
-            //Increments counter so that the viewnote.jsp is not displayed again
-            counter++;
-            
             //Load viewnote.jsp from this servlet
             getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response);
             return;
         }
         //Check if editnote.jsp should be displayed
-        else if (edit.equals("true")) {
+        else if (edit != null) {
             //Load editnote.jsp from this servlet
             getServletContext().getRequestDispatcher("/WEB-INF/editnote.jsp").forward(request, response);
             return;
@@ -68,8 +64,6 @@ public class NoteServlet extends HttpServlet {
         catch(Exception e) {
             e.getStackTrace();
         }
-        //Sets counter back to 0 so that viewnote.jsp is loaded
-        counter = 0;
         doGet(request, response);
     }
 }
